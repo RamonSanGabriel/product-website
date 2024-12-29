c75fb8430f7a44cdd08ed7d2db440983b01bf851
RamonSanGabriel <jeshero11@gmail.com>
2024-12-30 03:30:58 +0800

    navbar

--- product-website/src/App.jsx
+++ product-website/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div></div>
-      <h1>Proudct Website</h1>
+      <h2>CheekyChickShop</h2>
+      <NavBar />
     </>
   );
 }
