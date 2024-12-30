b124a94178bd394731c4f433ff983d7c922b5bc3
RamonSanGabriel <jeshero11@gmail.com>
2024-12-31 02:42:20 +0800

    bold logo

--- product-website/src/components/Header/Header.jsx
+++ product-website/src/components/Header/Header.jsx
@@ -5,7 +5,14 @@ const Header = () => {
   return (
     <div className={css.headerContainer}>
       <img className={css.headerLogo} src={logo} alt="Cheeky Chick Shop Logo" />
-      <h2>CheekyChickShop</h2>
+      <h2>
+        <b>C</b>
+        heeky
+        <b>C</b>
+        hick
+        <b>S</b>
+        hop
+      </h2>
     </div>
   );
 };
