d0be6ad6350dbbd3004358b3956e0ffb55dbb953
RamonSanGabriel <jeshero11@gmail.com>
2024-12-31 01:54:13 +0800

    social media links

--- product-website/src/components/Information/FollowUs/FollowUs.jsx
+++ product-website/src/components/Information/FollowUs/FollowUs.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import { FaFacebook, FaInstagramSquare, FaTwitterSquare } from 'react-icons/fa';
+import css from './FollowUs.module.css';
 
 const FollowUs = () => {
   return (
     <div>
       <h6>Follow us</h6>
+      <ul>
+        <li className={css.socialMedia}>
+          <span>
+            <FaFacebook />
+            &nbsp; <p>Facebook</p>
+          </span>
+        </li>
+        <li className={css.socialMedia}>
+          <span>
+            <FaInstagramSquare />
+            &nbsp; <p>Instagram</p>
+          </span>
+        </li>
+        <li className={css.socialMedia}>
+          <span>
+            <FaTwitterSquare />
+            &nbsp; <p>Twitter</p>
+          </span>
+        </li>
+      </ul>
     </div>
   );
 };
