a5b29925829363e423ea273a45dd8b3228589928
RamonSanGabriel <jeshero11@gmail.com>
2024-12-30 22:21:48 +0800

    logo src

--- product-website/src/components/Header/Header.jsx
+++ product-website/src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import css from './Header.module.css';
+import logo from '../../images/cheekyChickShop.jpg';
 
 const Header = () => {
   return (
     <div className={css.headerContainer}>
-      <img className={css.headerLogo} src="/cheekyChickShop.jpg" />
+      <img className={css.headerLogo} src={logo} alt="Cheeky Chick Shop Logo" />
       <h2>CheekyChickShop</h2>
     </div>
   );
