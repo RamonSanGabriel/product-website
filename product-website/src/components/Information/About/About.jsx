cb36e96342a32ac1b5f5db7815cb0fe28c63a335
RamonSanGabriel <jeshero11@gmail.com>
2024-12-31 19:24:21 +0800

    about us

--- product-website/src/components/Information/About/About.jsx
+++ product-website/src/components/Information/About/About.jsx
@@ -1,7 +1,26 @@
+import css from './About.module.css';
+
 const About = () => {
   return (
     <div>
       <h6>About us</h6>
+      <ul>
+        <li className={css.aboutUs}>
+          <span>
+            &nbsp; <p>Who we are</p>
+          </span>
+        </li>
+        <li className={css.aboutUs}>
+          <span>
+            &nbsp; <p>Instagram</p>
+          </span>
+        </li>
+        <li className={css.aboutUs}>
+          <span>
+            &nbsp; <p>Twitter</p>
+          </span>
+        </li>
+      </ul>
     </div>
   );
 };
