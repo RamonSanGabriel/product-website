f4fdb545a24f5870cdaeb503d0541278865196ab
RamonSanGabriel <jeshero11@gmail.com>
2024-12-31 19:36:40 +0800

    customer service

--- product-website/src/components/Information/CustomerService/CustomerService.jsx
+++ product-website/src/components/Information/CustomerService/CustomerService.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import css from './CustomerService.module.css';
 
 const CustomerService = () => {
   return (
     <div>
       <h6>Customer Service</h6>
+      <ul>
+        <li className={css.customerService}>
+          <span>
+            &nbsp; <p>Help centre</p>
+          </span>
+        </li>
+        <li className={css.customerService}>
+          <span>
+            &nbsp; <p>Return & Refund</p>
+          </span>
+        </li>
+        <li className={css.customerService}>
+          <span>
+            &nbsp; <p>Contact Us</p>
+          </span>
+        </li>
+      </ul>
     </div>
   );
 };
