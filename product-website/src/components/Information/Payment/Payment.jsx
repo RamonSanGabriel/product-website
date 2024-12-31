fbb7d683130f9e9faab8a8cf5a7f202d34207cb6
RamonSanGabriel <jeshero11@gmail.com>
2024-12-31 12:31:54 +0800

    payment logo

--- product-website/src/components/Information/Payment/Payment.jsx
+++ product-website/src/components/Information/Payment/Payment.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import css from './Payment.module.css';
+import bdo from '../../../images/bdoLogo.png';
+import bpi from '../../../images/bpi.png';
 
 const Payment = () => {
   return (
     <div>
       <h6>Payment</h6>
+      <ul>
+        <li className={css.socialMedia}>
+          <span>
+            <img className={css.paymentLogo} src={bdo} alt="" />
+            &nbsp; <p>Banco De Oro</p>
+          </span>
+        </li>
+        <li className={css.socialMedia}>
+          <span>
+            <img className={css.paymentLogo} src={bpi} alt="" />
+            &nbsp; <p>Bank of the Philippine Islands</p>
+          </span>
+        </li>
+      </ul>
     </div>
   );
 };
